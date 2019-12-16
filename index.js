c1511391d33c0b03014f3ba132c4e87202516e20
Ralf Schimmel <ralfschimmel@users.noreply.github.com>
2019-12-17 00:05:15 +0100

    Default to options.prepend instead of fingerprint.prepend

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = {
     this.app.options['asset-cache'].appName =
         this.app.name || this.app.options['asset-cache'].appName;
 
-    if (this.app.options.fingerprint && this.app.options.fingerprint.enabled) {
+    if (this.app.options.fingerprint && this.app.options.fingerprint.enabled && !this.app.options['asset-cache'].prepend) {
       this.app.options['asset-cache'].prepend = this.app.options.fingerprint.prepend;
     }
   },
